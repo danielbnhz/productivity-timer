544e1b97413c4c64a3328e9a0223c82d76d7a9b9
Daniel <danielbnhz@gmail.com>
2026-01-09 23:37:01 -0800

    adds logger function calls to main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from timer import Timer
 from storage import init_db, log_session
+from logger import logger
 import uuid
 import time
 
@@ -8,15 +9,20 @@ init_db()
 
 
 def main():
+    logger.info("App started")
+
     print("Running program \n")
     while input("Initiate task counter? (answer y or n) \n") == "y":
-
-        minutes = int(input("Enter the minutes you want to be productive for \n"))
         session_id = str(uuid.uuid4())
+        logger.info(f"Session started: {session_id}")
+
         start = int(time.time())
+        minutes = int(input("Enter the minutes you want to be productive for \n"))
         Timer(minutes)
         end = int(time.time())
         log_session(session_id, start, end)
+        duration = end - start 
+        logger.info(f"Session saved: {session_id} ({duration}s)")
 
 
 
